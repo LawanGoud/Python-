1329aecee1bed5fe828ed837b501b971032be940
LawanGoud <lavan.bairi@gmail.com>
2025-02-01 14:13:46 +0530

    Print in Reverse Order

---
+++ Print in Reverse Order/index.py
@@ -0,0 +1,6 @@
+# Write a program that reads two lines of input and prints those two lines in the reverse order. (Print the message given in the second line of input before the first line of input)
+
+word1 = input() # Input: Book
+word2 = input() # Input: Pen
+print(word2) # Output: Pen
+print(word1) # Output: Book
