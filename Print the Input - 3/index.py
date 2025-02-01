d627e58f4735c2268462d3db6f2a3031aa41c392
LawanGoud <lavan.bairi@gmail.com>
2025-02-01 14:06:37 +0530

    Print the Input - 3

---
+++ Print the Input - 3/index.py
@@ -0,0 +1,6 @@
+# Write a program that reads two words and prints the two words on two lines.
+
+word1 = input() # Input: Apple
+word2 = input() # Input: Banana
+print(word1) # Output: Apple
+print(word2) # Output: Banana
