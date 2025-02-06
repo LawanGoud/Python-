21a546dae0cbd1be0656eac30411bdaa352e4d9c
LawanGoud <lavan.bairi@gmail.com>
2025-02-06 23:01:31 +0530

    Division of two numbers

---
+++ CodingPractice - 1D/Division of two numbers/index.py
@@ -0,0 +1,11 @@
+# Write a program that reads two numbers A and B and prints the division of two numbers (A / B).
+
+a = input() # Input: 15
+b = input() # Input: 3
+
+# Convert to input to integers
+a = int(a)
+b = int(b)
+
+# Print the division of numbers
+print(a / b) # Output: 5.0
