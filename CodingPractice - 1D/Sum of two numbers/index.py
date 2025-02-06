f6c6b20faf08921af86187fbea44c9df6497c6bc
LawanGoud <lavan.bairi@gmail.com>
2025-02-06 22:56:06 +0530

    Sum of two numbers

---
+++ CodingPractice - 1D/Sum of two numbers/index.py
@@ -0,0 +1,11 @@
+# Write a program to print the sum of two integers inputs A and B.
+
+a = input() # Input: 2
+b = input() # Input: 3
+
+# Convert the input to integers
+a = int(a)
+b = int(b)
+
+# Print the sum of a and b
+print(a + b) # Output: 5
