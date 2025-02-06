5b8979637849a1a70508a80cade1e53c4f98f64c
LawanGoud <lavan.bairi@gmail.com>
2025-02-06 22:05:15 +0530

    Print in Reverse Order -2

---
+++ Assignment - 1A/Print in Reverse Order - 2/index.py
@@ -0,0 +1,8 @@
+# Write a program that reads two words A and B, and prints the given words in reverse order separated by ###
+
+a = input() # Input: Cat
+b = input() # Input: Rat
+
+print(b) # Output: Rat
+print("###") # Output: ###
+print(a) # Output : Cat
