62c535c1181f36de8a5d2b926b91f86b66af25b4
LawanGoud <lavan.bairi@gmail.com>
2025-02-07 11:17:25 +0530

    Percentage

---
+++ CodingPractice - 1D/Percentage/index.py
@@ -0,0 +1,8 @@
+# Write a program that reads a percentage `P` and prints the percentage `P` of the number 200.
+
+percentage = input() # Input: 50
+# Convert the percentage to a integer
+integer = int(percentage) / 100
+# Calculate 200 * integer
+result = 200 * integer
+print(result) # Output: 100.0
