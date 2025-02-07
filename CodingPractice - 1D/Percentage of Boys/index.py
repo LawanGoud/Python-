e51c92a5995da7349930eabf8aa20db85b86a793
LawanGoud <lavan.bairi@gmail.com>
2025-02-07 11:04:07 +0530

    Percentage of Boys

---
+++ CodingPractice - 1D/Percentage of Boys/index.py
@@ -0,0 +1,8 @@
+# Write a program that reads the percentage of girls in a class and prints the percentage of boys in the class.
+
+percentage_of_girls = input() # Input: 30
+percentage_of_girls = int(percentage_of_girls) # Convert input to integer
+# percentage_of_girls = 30
+# percentage_of_boys = 100 - percentage_of_girls
+percentage_of_boys = 100 - percentage_of_girls
+print(percentage_of_boys) # Output: 70
