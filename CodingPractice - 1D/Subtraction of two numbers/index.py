875ee84b3d910667353a8f780924282057ad34d3
LawanGoud <lavan.bairi@gmail.com>
2025-02-07 11:00:10 +0530

    Subtraction of two numbers

---
+++ CodingPractice - 1D/Subtraction of two numbers/index.py
@@ -0,0 +1,5 @@
+# Write a program that reads two numbers A and B and prints the subtraction of two numbers (A - B).
+
+a = input() # Input: 15.55
+b = input() # Input: 6.23
+print(float(a) - float(b)) # Output: 9.32
