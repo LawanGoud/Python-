f8c6dfab8080e3c13f1f16dbbaba24aedc334884
LawanGoud <lavan.bairi@gmail.com>
2025-02-07 11:12:56 +0530

    Kilometer to meter

---
+++ CodingPractice - 1D/Kilometers to Meters/index.py
@@ -0,0 +1,8 @@
+# Write a program to take the number of kilometers as input and convert into meters and print the number of meters.
+
+number = input() # Input: 1.2
+number = float(number) # Convert to float
+# Convert kilometers to meters
+meters = number * 1000 # Convert to integer
+meters = int(meters)
+print(meters) # Output: 1200
