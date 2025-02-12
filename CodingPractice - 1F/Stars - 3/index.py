a0d1436908da6aaa9622cbbbde6010427ed193b4
LawanGoud <lavan.bairi@gmail.com>
2025-02-12 11:46:10 +0530

    stars - 3

---
+++ CodingPractice - 1F/Stars - 3/index.py
@@ -0,0 +1,7 @@
+# Write a program that reads a number N and prints three lines with each line containing N stars (\*)
+
+number = int(input()) # Input: 4
+
+print("* " * number) # * * * *
+print("* " * number) # * * * *
+print("* " * number) # * * * *
