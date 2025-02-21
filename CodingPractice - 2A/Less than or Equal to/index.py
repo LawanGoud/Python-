502eb84c2d56b1f67d236c6e698228c8860975f1
LawanGoud <lavan.bairi@gmail.com>
2025-02-21 13:56:23 +0530

    Less than or Equal to

---
+++ CodingPractice - 2A/Less than or Equal to/index.py
@@ -0,0 +1,7 @@
+# Write a program that reads two numbres and checks if the first number is less than or equal to the second number.
+
+first_number = int(input()) # 2
+second_number = int(input()) # 2.3
+
+result = first_number <= second_number
+print(result) # Output: True
